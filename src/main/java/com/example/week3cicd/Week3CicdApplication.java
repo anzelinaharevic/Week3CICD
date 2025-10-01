3bf3d6fab08e47b103f3bacaa72ec0206565ba40
ANZELINAHAREVICA-STU <g00414991@atu.ie>
2025-10-01 14:49:29 +0100

    Creating project

---
+++ src/main/java/com/example/week3cicd/Week3CicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.week3cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3CicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/week3cicd/Week3CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week3cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3CicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
